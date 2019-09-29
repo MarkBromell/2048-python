d08a41b37fbca0a81ec5185ed1f79ddc196c115b
Mark Bromell <33425418+MarkBromell@users.noreply.github.com>
2019-09-29 18:16:43 +0100

    Basic functionality of Cell class has been created

--- cell.py
+++ cell.py
@@ -1,2 +1,29 @@
+import random
+
+
 class Cell:
-    pass
+    def __init__(self, value=0):
+        self._value = value
+
+    def __str__(self):
+        return str(self._value)
+
+    def get_value(self):
+        return self._value
+
+    def increase_value(self):
+        self._value *= 2
+
+    def initialize_value(self):
+        random_number = random.randint(1, 6)
+
+        if random_number >= 1 or random_number <= 5:
+            self._value = 2
+        else:
+            self._value = 4
+
+    def can_combine(self, other):
+        if self._value == other.get_value():
+            return True
+        else:
+            return False
--- tests/test_cell.py
+++ tests/test_cell.py
@@ -3,5 +3,30 @@ import cell
 
 
 class TestCell(unittest.TestCase):
-    def test_(self):
-        pass
+    def test_str(self):
+        my_cell = cell.Cell(2)
+        self.assertEquals(str(my_cell), "2")
+
+    def test_get_value(self):
+        my_cell = cell.Cell(2)
+        self.assertEquals(my_cell.get_value(), 2)
+
+    def test_increase_value(self):
+        my_cell = cell.Cell(4)
+        my_cell.increase_value()
+        self.assertEquals(my_cell.get_value(), 8)
+
+    def test_initialize_value(self):
+        my_cell = cell.Cell()
+        my_cell.initialize_value()
+        self.assertTrue(my_cell.get_value() == 2 or my_cell.get_value() == 4)
+
+    def test_can_combine_invalid(self):
+        cell1 = cell.Cell(2)
+        cell2 = cell.Cell(4)
+        self.assertFalse(cell1.can_combine(cell2))
+
+    def test_can_combine_valid(self):
+        cell1 = cell.Cell(4)
+        cell2 = cell.Cell(4)
+        self.assertTrue(cell1.can_combine(cell2))
