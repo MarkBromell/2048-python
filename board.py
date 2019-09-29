e88878c5efa080e5f9ee65cab126b033001afdbd
Mark Bromell <33425418+MarkBromell@users.noreply.github.com>
2019-09-29 19:35:14 +0100

    Implemented most of the Board class functionality

--- board.py
+++ board.py
@@ -2,4 +2,68 @@ import cell
 
 
 class Board:
-    pass
+    __cell_width = 5
+
+    def __init__(self, height=4, width=4):
+        self._height = height
+        self._width = width
+        self._cells = []
+
+        for row in range(self._height):
+            new_row = []
+            for col in range(self._width):
+                new_row.append(0)
+            self._cells.append(new_row)
+
+    def __str__(self):
+        string = self._top_border()
+
+        for row in range(self._height):
+            string += '│'
+            for col in range(self._width):
+                string += self._str_cell(self._cells[row][col])
+
+            string += '\n'
+            if row != self._height - 1:
+                string += self._middle_border()
+
+        string += self._bottom_border()
+        return string
+
+    def get_width(self):
+        return self._width
+
+    def get_height(self):
+        return self._height
+
+    # region functions for drawing the board
+    def _str_cell(self, value):
+        return ('{:^' + self.__cell_width + '}│').format(str(value))
+
+    def _top_border(self):
+        string = '┌'
+
+        for i in range(self._width - 1):
+            string += ('─' * self.__cell_width) + '┬'
+        string += ('─' * self.__cell_width) + '┐\n'
+
+        return string
+
+    def _middle_border(self):
+        string = '├'
+
+        for i in range(self._width - 1):
+            string += ('─' * self.__cell_width) + '┼'
+        string += ('─' * self.__cell_width) + '┤\n'
+
+        return string
+
+    def _bottom_border(self):
+        string = '└'
+
+        for i in range(self._width - 1):
+            string += ('─' * self.__cell_width) + '┴'
+        string += ('─' * self.__cell_width) + '┘\n'
+
+        return string
+    # endregion
--- tests/test_board.py
+++ tests/test_board.py
@@ -3,5 +3,45 @@ import board
 
 
 class TestBoard(unittest.TestCase):
-    def test_(self):
-        pass
+    def test_str(self):
+        my_board = board.Board(4, 4)
+        string = (
+            '┌─────┬─────┬─────┬─────┐\n'
+            '│  0  │  0  │  0  │  0  │\n'
+            '├─────┼─────┼─────┼─────┤\n'
+            '│  0  │  0  │  0  │  0  │\n'
+            '├─────┼─────┼─────┼─────┤\n'
+            '│  0  │  0  │  0  │  0  │\n'
+            '├─────┼─────┼─────┼─────┤\n'
+            '│  0  │  0  │  0  │  0  │\n'
+            '└─────┴─────┴─────┴─────┘\n'
+        )
+        self.assertEquals(str(my_board), string)
+
+    def test_get_height(self):
+        my_board = board.Board(4, 3)
+        self.assertEquals(my_board.get_height(), 4)
+
+    def test_get_width(self):
+        my_board = board.Board(3, 4)
+        self.assertEquals(my_board.get_width(), 4)
+
+    def test_str_cell(self):
+        my_board = board.Board(4, 4)
+        string = '  0  │'
+        self.assertEquals(my_board._str_cell('0'), string)
+
+    def test_top_border(self):
+        my_board = board.Board(4, 4)
+        string = '┌─────┬─────┬─────┬─────┐\n'
+        self.assertEquals(my_board._top_border(), string)
+
+    def test_middle_border(self):
+        my_board = board.Board(4, 4)
+        string = '├─────┼─────┼─────┼─────┤\n'
+        self.assertEquals(my_board._middle_border(), string)
+
+    def test_bottom_border(self):
+        my_board = board.Board(4, 4)
+        string = '└─────┴─────┴─────┴─────┘\n'
+        self.assertEquals(my_board._bottom_border(), string)
